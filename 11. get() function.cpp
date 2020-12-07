d1b4b4ea5e50b11a3e202c7b19575172bb4d81f0
Eshita Goel <66893825+eshitagoel@users.noreply.github.com>
2020-12-07 11:11:21 +0530

    Add files via upload

---
+++ 11. get() function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char a,b,c;
+    cin.get(a);
+    cin.get(b);
+    cin.get(c);
+    cout<<a<<b<<c;
+    cout<<endl<<endl;
+    
+    return 0;
+}
