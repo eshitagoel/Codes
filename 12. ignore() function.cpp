292b1edd6f18d3f514be9214e0ec1efb97edcf0e
Eshita Goel <66893825+eshitagoel@users.noreply.github.com>
2020-12-08 08:50:32 +0530

    Add files via upload

---
+++ 12. ignore() function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char a,b;
+    cin>>a;
+    cin.ignore(100,'\n');
+    cin>>b;
+    cout<<endl<<"Output : "<<a<<" "<<b;
+    
+    cout<<endl<<endl;
+    
+    return 0;
+}
